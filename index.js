8057bd868a3ba314f99a783e6846ce1ad651373c
andrewgremlich <andrew.gremlich@gmail.com>
2020-04-13 16:06:07 -0600

    Updating script to make _index.md files for hugo.

--- index.js
+++ index.js
@@ -16,11 +16,27 @@ const loadSection = async (url) => {
   const sectionData = await fetch(`${CHURCH_URL}${url}`);
   const parsedTextData = sectionData.text();
   return parsedTextData;
-}
-    
+};
+
+const makeFrontMatter = (names, ...params) => {
+  const toBeStringFrontMatter = names
+    .filter((name) => name)
+    .map((name, index) => `${name}${params[index]}`)
+    .join("\n");
+
+  return `---
+${toBeStringFrontMatter}
+---
+
+`;
+};
+
 // parse the content of the chapter page and then add it to
 // a markdown file.
-const getPage = (url, { title, href, orderNumber }) => async () => {
+const getPage = (
+  url,
+  { title, href, orderNumber, sectionOrderNumber }
+) => async () => {
   const section = await loadSection(url);
   const $ = cheerio.load(section);
 
@@ -29,19 +45,26 @@ const getPage = (url, { title, href, orderNumber }) => async () => {
 
   const text = $(".renderFrame-1yHgQ .body-block").text();
 
-  const chapterSummary = $("#study_summary1").text();
-  const frontMatter = `---
-title: ${title}
-date: ${new Date()}
-draft: false
-description: "${chapterSummary}"
+  const dominant = $(".dominant").text();
+  const isRightFolder = href.split(/\//g);
 
-order: ${orderNumber}
----
+  if (sectionOrderNumber && isRightFolder.length === 6 && dominant) {
+    const indexOfBookFrontMatter = makeFrontMatter`title: ${dominant}date: ${new Date()}order: ${sectionOrderNumber}`;
+    const indexFile = `${indexOfBookFrontMatter}## ${dominant}`;
 
-`;
+    const dirToWrite = [...isRightFolder];
+    dirToWrite.splice(5, 1, "");
+    const joinToHref = "." + dirToWrite.join("/");
+
+    await mkdirp(joinToHref);
+    await writeFile(joinToHref + "_index.md", indexFile);
+  }
 
   if (text) {
+    const chapterSummary = $("#study_summary1").text();
+    const frontMatter = makeFrontMatter`title: ${title}date: ${new Date()}description: ${chapterSummary}order: ${
+      sectionOrderNumber || orderNumber
+    }`;
     const prettyMD = prettier.format(text, { parser: "markdown" });
     const file = frontMatter + prettyMD.replace(/[:\.;!?\),](?=\d)/g, ".\n\n");
 
@@ -51,13 +74,14 @@ order: ${orderNumber}
     const joinedFilePath = splitFilePath.join("/");
 
     // for debugging purposes.
-    console.log("Writing page", title);
-    return;
+    // console.log("Writing page", title);
 
-    // await mkdirp(joinedFilePath);
-    // await writeFile(`${joinedFilePath}/${chapterName}.md`, file);
-    // return;
+    await mkdirp(joinedFilePath);
+    await writeFile(`${joinedFilePath}/${chapterName}.md`, file);
   }
+
+  //NEEDED for promise chain to work.
+  return;
 };
 
 // Go through the list of chapters with the list that is open.
@@ -79,7 +103,7 @@ const parseThroughList = ($, listWithBook, PromiseChain) => {
 };
 
 // Go to the chapter URL
-const navigateToPage = async (url, PromiseChain) => {
+const navigateToPage = async (url, PromiseChain, sectionOrderNumber) => {
   const section = await loadSection(url);
 
   const $ = cheerio.load(section);
@@ -94,16 +118,18 @@ const navigateToPage = async (url, PromiseChain) => {
       const [noQueryParams] = hrefForPage.split("?");
       const orderNumber = index + 1;
 
+      PromiseChain = PromiseChain.then(
+        getPage(hrefForPage, {
+          title: pageTitle,
+          href: noQueryParams,
+          orderNumber,
+          sectionOrderNumber,
+        })
+      );
+
+      //WILL overwrite all 1.md files from previous getPage call.
       if (listWithBook.length > 0) {
         parseThroughList($, listWithBook, PromiseChain);
-      } else {
-        PromiseChain = PromiseChain.then(
-          getPage(hrefForPage, {
-            title: pageTitle,
-            href: noQueryParams,
-            orderNumber,
-          })
-        );
       }
     }
   });
@@ -119,9 +145,10 @@ const navigateManifest = async (url) => {
 
   $(".manifest a").each((index, element) => {
     const hrefValue = $(element).attr("href");
+    const sectionOrderNumber = index + 1;
 
     if (hrefValue) {
-      navigateToPage(hrefValue, PromiseChain);
+      navigateToPage(hrefValue, PromiseChain, sectionOrderNumber);
     }
   });
 };
@@ -135,7 +162,14 @@ const navigateThroughTiles = async (url) => {
 
     // This index number pertains to whatever book of scripture found
     // in GOSPEL_LIBRARY_URL
-    if (index === 2 && hrefValue) {
+    /**
+     * 0 = OT
+     * 1 = NT
+     * 2 = BOM
+     * 3 = D&C
+     * 4 = PGP
+     */
+    if (index === 0 && hrefValue) {
       navigateManifest(hrefValue);
     }
   });
